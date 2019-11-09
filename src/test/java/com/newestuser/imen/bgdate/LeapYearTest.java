73272a557ab17df79f380d475826a0e4a26df1db
Ivo Staynov <staynov123@abv.bg>
2019-11-09 19:26:25 +0200

    Implementing leapyear test

---
+++ src/test/java/com/newestuser/imen/bgdate/LeapYearTest.java
@@ -0,0 +1,25 @@
+package com.newestuser.imen.bgdate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeapYearTest {
+
+    @Test
+    public void findIsLeapYear() {
+        assertFalse(findIsLeapYear(1800));
+        assertTrue(findIsLeapYear(2020));
+    }
+
+    private static boolean findIsLeapYear(int currentYear) {
+        if (currentYear % 4 == 0 && currentYear % 100 != 0) {
+            return true;
+        }
+        if (currentYear % 100 == 0 && currentYear % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
