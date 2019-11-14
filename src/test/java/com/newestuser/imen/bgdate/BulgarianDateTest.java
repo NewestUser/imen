6555f648e1c09353b2046982f79204d57ce24de9
Ivo Staynov <staynov123@abv.bg>
2019-11-14 22:02:13 +0200

    leapyear check

--- src/test/java/com/newestuser/imen/bgdate/BulgarianDateTest.java
+++ src/test/java/com/newestuser/imen/bgdate/BulgarianDateTest.java
@@ -25,4 +25,39 @@ public class BulgarianDateTest {
         calendar.set(Calendar.DAY_OF_MONTH, 1);
         return calendar.getTime();
     }
+
+    @Test
+    public void checkLeapYear(){
+        Date gregorianDate = secondOfJanuary(2020);
+        BulgarianDate bulgarianDate = new BulgarianDate(gregorianDate);
+        String formatedDate = bulgarianDate.format(bulgarianDate);
+        assertEquals(formatedDate, "11 Алем, 6788 Сомор");
+
+    }
+
+    private Date secondOfJanuary(int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 2);
+        return calendar.getTime();
+    }
+
+    @Test
+    public void checkLeapYearSecondTest(){
+        Date gregorianDate = thirdOfMarch(1800);
+        BulgarianDate bulgarianDate = new BulgarianDate(gregorianDate);
+        String formatedDate = bulgarianDate.format(bulgarianDate);
+        assertEquals(formatedDate, "10 Читем, 6568 Бечин");
+
+    }
+
+    private Date thirdOfMarch(int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, Calendar.MARCH);
+        calendar.set(Calendar.DAY_OF_MONTH, 3);
+        return calendar.getTime();
+    }
+
 }
